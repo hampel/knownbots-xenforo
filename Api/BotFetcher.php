337cd80bd0efa4da070e7d4fee6b7735700c3882
Simon Hampel <simon@hampelgroup.com>
2023-08-24 21:46:51 +1000

    rewrite bot fetcher to use trustedUrl when manually configured - for testing purposes when using a .local domain for the API

--- Api/BotFetcher.php
+++ Api/BotFetcher.php
@@ -1,35 +1,59 @@
 <?php namespace Hampel\KnownBots\Api;
 
-use Hampel\KnownBots\SubContainer\Cache;
 use Hampel\KnownBots\SubContainer\Log;
 use XF\Util\File;
 
 class BotFetcher
 {
-    protected $url = "https://knownbots.hampel.io/api/bots";
-
     /**
      * @var \XF\App
      */
     protected $app;
+
+    /** @var string */
+    protected $baseUrl = 'https://knownbots.hampel.io/api';
+
+    /** @var bool */
+    protected $trustedUrl = false;
 
     public function __construct(\XF\App $app)
     {
         $this->app = $app;
     }
 
-    public function fetchBots($force)
+    /**
+     * @param $url string Base URL for API calls
+     * @param $trusted bool Should the base url be considered  trusted? Default is false, set to true only for testing environments
+     * @return void
+     */
+    public function setUrl($url, $trusted = false)
     {
-        $cache = $this->getCache();
+        $this->baseUrl = rtrim($url, '/');
+        $this->trustedUrl = $trusted;
+    }
+
+    public function fetch($lastChecked, $force = false)
+    {
         $log = $this->getLogger();
 
-        $url = $this->buildUrl($force ? 0 : $cache->getLastChecked());
+        $url = $this->buildUrl($force ? 0 : $lastChecked);
 
         $log->debug('Fetching updated bots', compact('url'));
 
         $destination = File::getNamedTempFile(sprintf("knownbots-%s.json", \XF::$time));
 
-        if(!$response = $this->app->http()->reader()->getUntrusted($url, [], $destination, [], $error))
+        if ($this->trustedUrl)
+        {
+            // only used when we set a manual API url, used to bypass checks so we can use localhost for testing
+            $response = $this->app->http()->reader()->get($url, [], $destination, [], $error);
+        }
+        else
+        {
+            // treat URLs as untrusted by default, will run through proxy server if configured
+            $response = $this->app->http()->reader()->getUntrusted($url, [], $destination, [], $error);
+        }
+
+        if(!$response)
         {
             $log->error('Error fetching bots', compact('url', 'destination', 'error'));
             \XF::logError($error);
@@ -41,7 +65,7 @@ class BotFetcher
 
     protected function buildUrl($since)
     {
-        return $this->url . ($since > 0 ? ("?" . http_build_query(compact('since'))) : '');
+        return "{$this->baseUrl}/bots" . ($since > 0 ? ("?" . http_build_query(compact('since'))) : '');
     }
 
     /**
@@ -50,13 +74,5 @@ class BotFetcher
     protected function getLogger()
     {
         return $this->app['knownbots.log'];
-    }
-
-    /**
-     * @return Cache
-     */
-    protected function getCache()
-    {
-        return $this->app['knownbots.cache'];
     }
 }
