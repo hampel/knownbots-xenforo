c76a17c3fe553507c93a79e6fb516baeca5e2672
Simon Hampel <simon@hampelgroup.com>
2025-12-05 12:04:25 +1100

    metadata updates

---
+++ _output/option_hint.php
@@ -0,0 +1,22 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property array{enabled: string, email: string}|null $knownbotsEmailUserAgents Email user agents
+ * @property bool|null $knownbotsFetchNewBots Fetch new bots
+ * @property array{enabled: bool, validation_token: bool, api_token: bool}|null $knownbotsSendUserAgents Send user agents via API
+ * @property bool|null $knownbotsShowBotStats Show robot statistics
+ * @property array{enabled: string, days: string}|null $knownbotsStoreUserAgents Store user agents in database and purge after:
+ */
+class Options
+{
+}
